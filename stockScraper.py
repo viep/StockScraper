6535559cb03d2bcb77900257504f803a041092ec
jayasudha jayakumaran <jjayakum@andrew.cmu.edu>
2016-05-18 17:05:51 -0700

    count of tickers to be cached taken in as argument

--- stockScraper.py
+++ stockScraper.py
@@ -1,10 +1,9 @@
-
 from urllib2 import Request,urlopen,URLError
+import sys
 import mysql.connector
 import time
 import datetime
 import os
-import pickle
 import pandas as pd
 import threading
 from pandas.io import sql
@@ -15,7 +14,6 @@ def getPrice(tickers):
 	req = link+arguments+tickers
 	request= Request(req)
 	columns = ['a','a2','b','b2','b3','b4','c','c1','c3','c4','c6','c8','d','d1','d2','e','e1','e7','e8','e9','g','h','j','k','g1','g3','g4','g5','g6','i','i5','j1','j3','j4','j5','j6','k1','k2','k4','k5','l','l1','l2','l3','m','m2','m3','m4','m5','m6','m7','m8','n','n4','o','p','p1','p2','p5','p6','q','r','r1','r2','r5','r6','r7','s','s1','s7','t1','t7','t8','v','v1','v7','w','w1','w4','x','y']
-
 	try:
 		frameEach = pd.DataFrame(index = None)
 		resp = urlopen(request)
@@ -32,7 +30,6 @@ def getPrice(tickers):
 	frameEach['tickerName']=tickerFrame
 	st = getCurrentTimestamp()
 	frameEach['timestamp'] = st
-	print frameEach
 	global frame
 	frame = frame.append(frameEach)
 	return
@@ -78,22 +75,37 @@ def concatTickers(filename):
 	return tickers
 
 def writeToDB(frame):
+	print "writeToDB, length of frame",len(frame)
 	engine = create_engine('mysql+mysqlconnector://honeybee:honeybee@127.0.0.1/stocksdb', echo=False)
 	frame.to_sql(name='financialTable', con=engine, if_exists = 'append', index=False)
+	engine.dispose()
+	frame = pd.DataFrame()
+	return frame
 
 def job():
-	#threading.Timer(1,job).start()
-
-	#runs twice for 300 companies
+	global count
+	global countToWrite
 	while(True):
 		global tickers
 		getPrice(tickers)
-		if(len(frame)==600):
-			print "len=600"
-			return
-		time.sleep(10)
+		countToWrite+=len(frame)
+		if(countToWrite>=float(count)):
+			global frame
+			global countToWrite
+			countToWrite = 0
+			frame=writeToDB(frame)
+		time.sleep(50)
+	#threading.Timer(1,job).start()
 
 def main():
+	global count
+	global countToWrite
+	countToWrite=0
+	try:
+		count = sys.argv[1]
+	except:
+		print "usage | python stockScraper.py [count of tickers cached]"
+		sys.exit(2)
 	global frame
 	frame = pd.DataFrame()
 	filename = 'tickers300.txt'
@@ -103,7 +115,7 @@ def main():
 #	frame.to_csv('out.csv',sep = ',')
 	assert frame['s'].all() == frame['tickerName'].all()
 	frame = frame.astype(object).where(pd.notnull(frame), None)
-	writeToDB(frame)
+	
 
 if __name__ == "__main__": main()
 
